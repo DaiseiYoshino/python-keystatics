888053258c43f152da3a5c1ec90e6c3e73eb5549
Daianywhere <qwerty190854@goo.jp>
2022-02-15 23:17:44 +0900

    javascript組み込みができることを試験・実証

--- reporter/main.ts
+++ reporter/main.ts
@@ -2,10 +2,15 @@ import todayString from './util/todayString.ts';
 
 let filePath: string = `../LOG/report-${todayString()}.html`;
 
+const testFunc = (): void => alert('Hello, this is a test!');
+
 let html: string = `
 <!doctype html>
 <html>
   <head>
+    <script type="text/javascript">
+      window.onload=${testFunc.toString()};
+    </script>
   </head>
   <body>
     <p>Hello!</p>
