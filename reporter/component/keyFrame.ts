a4e06cf3057cd9918471ab9b50ddf2db0f45a5a0
Daianywhere <qwerty190854@goo.jp>
2022-02-20 23:46:06 +0900

    keyBoxの中身が肥大化したので切り出す

--- reporter/component/keyFrame.ts
+++ reporter/component/keyFrame.ts
@@ -1,7 +1,21 @@
+const keyBox = (key: string): string => {
+  return `
+    <div
+      class="key-box"
+      onmouseover="mouseOverFunc(this)"
+      onmouseout="mouseOutFunc(this)"
+    >
+      <span class="key-char">
+        ${key}
+      </span>
+    </div>
+  `;
+};
+
 export default (keyInfo: {width:number, keys:string[]}): string => {
   return `
     <div class="key-frame" style="width:${keyInfo.width*60}px;">
-      ${keyInfo.keys.map(key => `<div class="key-box" onmouseover="mouseOverFunc(this)" onmouseout="mouseOutFunc(this)"><span class="key-char">${key}</span></div>`).join('')}
+      ${keyInfo.keys.map(keyBox).join('')}
     </div>
   `;
 };
