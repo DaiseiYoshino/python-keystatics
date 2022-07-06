a680eb807816fb4e57b20d38dff356ef3574ea29
Daianywhere <qwerty190854@goo.jp>
2022-07-07 00:47:02 +0900

    add type

--- reporter/types/types.ts
+++ reporter/types/types.ts
@@ -12,3 +12,10 @@ export type keyRowInfo = {
   padding?: number
   keys: keyInfo[]
 };
+
+export type keyTypes = Record<string, number>;
+
+export type keyBoardSettings = {
+  padding?: number,
+  keys: {width?: number, keys: string[]}[]
+}[];
