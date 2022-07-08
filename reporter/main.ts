8c9bc80995087b613ec70a8f7d00771a3b316c45
Daianywhere <qwerty190854@goo.jp>
2022-07-09 07:57:00 +0900

    create style block

--- reporter/main.ts
+++ reporter/main.ts
@@ -4,76 +4,80 @@ import todayString from './util/todayString.ts';
 
 const filePath = `../LOG/report-${todayString()}.html`;
 
+const stylePart = `
+<style>
+  div.key-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+  }
+
+  div.key-pad {
+    height: 60px;
+
+    box-sizing: border-box;
+  }
+
+  div.key-frame {
+    width: 60px;
+    height: 60px;
+
+    border: thick outset #aaaaaa;
+
+    box-sizing: border-box;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+  }
+
+  div.key-box {
+    flex-grow: 1;
+    width: 50%;
+    border: medium solid #cccccc;
+    box-sizing: border-box;
+    position: relative;
+  }
+
+  span.key-char {
+    font-weight: bold;
+
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    top: 20%;
+    text-align: center;
+  }
+
+  div.balloon {
+    display: none;
+
+    top: 30px;
+    left: 20px;
+    width: 100px;
+    height: 50px;
+    border: thin solid #aaaaff;
+    background-color: #eeeeee;
+    z-index: 1;
+
+    position: absolute;
+  }
+
+  div.balloon span {
+    top: 10px;
+    left: 10px;
+
+    position: absolute;
+  }
+</style>
+`;
+
 const html = `
 <!doctype html>
 <html>
   <head>
-    <style>
-      div.key-row {
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: flex-start;
-      }
-
-      div.key-pad {
-        height: 60px;
-
-        box-sizing: border-box;
-      }
-
-      div.key-frame {
-        width: 60px;
-        height: 60px;
-
-        border: thick outset #aaaaaa;
-
-        box-sizing: border-box;
-
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: flex-start;
-      }
-
-      div.key-box {
-        flex-grow: 1;
-        width: 50%;
-        border: medium solid #cccccc;
-        box-sizing: border-box;
-        position: relative;
-      }
-
-      span.key-char {
-        font-weight: bold;
-
-        width: 100%;
-        height: 100%;
-
-        position: absolute;
-        top: 20%;
-        text-align: center;
-      }
-
-      div.balloon {
-        display: none;
-
-        top: 30px;
-        left: 20px;
-        width: 100px;
-        height: 50px;
-        border: thin solid #aaaaff;
-        background-color: #eeeeee;
-        z-index: 1;
-
-        position: absolute;
-      }
-
-      div.balloon span {
-        top: 10px;
-        left: 10px;
-
-        position: absolute;
-      }
-    </style>
+    ${stylePart}
     <script type="text/javascript">
       const mouseOverFunc = ${onMouseOverFunc.toString()};
       const mouseOutFunc = ${onMouseOutFunc.toString()};
