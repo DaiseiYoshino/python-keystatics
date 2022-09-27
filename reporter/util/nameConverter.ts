604a792c9d5d2420211cabac1ae4974750a29764
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2022-09-27 18:33:57 +0900

    固有にキー名を設定できるようにする

--- reporter/util/nameConverter.ts
+++ reporter/util/nameConverter.ts
@@ -1,6 +1,19 @@
 import { keyInfo, keyRowInfo, singleChar } from "../types/types.ts";
 
 const convertName = (keyName:string):string => {
+  // 固有で設定したい判定
+  const dict:Record<string, string> = {
+    'Key.up': '↑',
+    'Key.down': '↓',
+    'Key.left': '←',
+    'Key.right': '→',
+    'Key.enter': '↵'
+  }
+  if (dict[keyName]) {
+    return dict[keyName];
+  }
+
+  // ここからその他の判定
   // 「'」で囲われている箇所のマッチ
   const quotedExp = /'(.+)'/;
   if (keyName.match(quotedExp)) {
