c15b44d7718be11a4fe72839d3d45e17a23251d7
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2022-09-27 18:14:58 +0900

    名称変換器を作成

--- reporter/util/nameConverter.ts
+++ reporter/util/nameConverter.ts
@@ -1,4 +1,4 @@
-import { keyRowInfo } from "../types/types";
+import { keyInfo, keyRowInfo, singleChar } from "../types/types";
 
 const convertName = (keyName:string):string => {
   // 「'」で囲われている箇所のマッチ
@@ -19,6 +19,31 @@ const convertName = (keyName:string):string => {
 
 export default (keyboard: keyRowInfo[]): keyRowInfo[] => {
   let ret: keyRowInfo[] = [];
+
+  for (const row of keyboard) {
+    let retKeys:keyInfo[] = [];
+
+    for (const keycap of row.keys) {
+      let retChars:singleChar[] = [];
+
+      for (const char of keycap.chars) {
+        retChars.push({
+          ...char,
+          name: convertName(char.name),
+        })
+      }
+
+      retKeys.push({
+        ...keycap,
+        chars: retChars
+      });
+    }
+
+    ret.push({
+      ...row,
+      keys: retKeys
+    });
+  }
 
   return ret;
 }
