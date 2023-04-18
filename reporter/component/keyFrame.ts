c6306d8f0a9b738bb019ec68106ba886373dce3d
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2023-04-18 15:08:20 +0900

    move function keyBox to SingleCharInfo method

--- reporter/component/keyFrame.ts
+++ reporter/component/keyFrame.ts
@@ -1,28 +1,9 @@
 import {keyInfo} from '../types/types.ts'
-import SingleCharInfo from '../lib/singleCharInfo.ts';
-
-const keyBox = (char: SingleCharInfo): string => {
-  return `
-    <div
-      class="key-box"
-      onmouseover="mouseOverFunc(this)"
-      onmouseout="mouseOutFunc(this)"
-      style="background-color: ${char.color}"
-    >
-      <span class="key-char">
-        ${char.name}
-      </span>
-      <div class="balloon">
-        <span>${char.name}: ${char.count}</span>
-      </div>
-    </div>
-  `;
-};
 
 export default (key: keyInfo): string => {
   return `
     <div class="key-frame" style="width:${(key.width ?? 1)*60}px;">
-      ${key.chars.map(keyBox).join('')}
+      ${key.chars.map(char => char.toElem()).join('')}
     </div>
   `;
 };
--- reporter/lib/singleCharInfo.ts
+++ reporter/lib/singleCharInfo.ts
@@ -8,6 +8,24 @@ class SingleCharInfo {
     this.count = params.count;
     this.color = params.color;
   }
+
+  toElem(): string {
+    return `
+    <div
+      class="key-box"
+      onmouseover="mouseOverFunc(this)"
+      onmouseout="mouseOutFunc(this)"
+      style="background-color: ${this.color ?? '#000'}"
+    >
+      <span class="key-char">
+        ${this.name}
+      </span>
+      <div class="balloon">
+        <span>${this.name}: ${this.count}</span>
+      </div>
+    </div>
+  `;
+  }
 }
 
 export default SingleCharInfo;
