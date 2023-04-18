212e4577d73ecf53735d540038df604ea7c10e56
Daisei Yoshino <daisei.yoshino@sencorp.co.jp>
2023-04-18 17:21:05 +0900

    alter keyFrame.ts type

--- reporter/component/keyFrame.ts
+++ reporter/component/keyFrame.ts
@@ -1,6 +1,6 @@
-import {keyInfo} from '../types/types.ts'
+import SingleKeyInfo from '../lib/singleKeyInfo.ts'
 
-export default (key: keyInfo): string => {
+export default (key: SingleKeyInfo): string => {
   return `
     <div class="key-frame" style="width:${(key.width ?? 1)*60}px;">
       ${key.chars.map(char => char.toElem()).join('')}
