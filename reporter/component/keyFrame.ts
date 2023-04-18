867e7778376919f2f3bad38f180a7baaf61c7e19
Daianywhere <qwerty190854@goo.jp>
2023-04-18 20:28:56 +0900

    remove function keyFrame

--- reporter/component/keyFrame.ts
+++
@@ -1,9 +0,0 @@
-import SingleKeyInfo from '../lib/singleKeyInfo.ts'
-
-export default (key: SingleKeyInfo): string => {
-  return `
-    <div class="key-frame" style="width:${(key.width ?? 1)*60}px;">
-      ${key.chars.map(char => char.toElem()).join('')}
-    </div>
-  `;
-};
--- reporter/component/keyRow.ts
+++ reporter/component/keyRow.ts
@@ -1,12 +1,11 @@
 import KeyRowInfo from '../lib/keyRowInfo.ts';
-import keyFrame from './keyFrame.ts';
 import keyPad from './keyPad.ts';
 
 export default (data: KeyRowInfo): string => {
   return `
     <div class="key-row">
       ${data.padding ? keyPad(data.padding) : ''}
-      ${data.keys.map(keyFrame).join('')}
+      ${data.keys.map(key => key.toElem()).join('')}
     </div>
   `
 };
